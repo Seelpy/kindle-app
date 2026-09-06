f07a2faa10d3939301807165d37d24d299de4e9c
Seelpy <71544630+Seelpy@users.noreply.github.com>
2026-09-06 22:18:46 +0300

    Remove Kindle-side app

--- koreader/dalshe.koplugin/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-
-return {
-    name = "dalshe",
-    fullname = _("Ещё пять"),
-    description = _("Спокойные короткие сеансы чтения и локальная синхронизация с Mac."),
-}
